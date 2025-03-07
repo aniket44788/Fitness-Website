f790efcc725bf186ec6a864fb13bef81c5a002f9
aniket44788 <darkpanda44788@gmail.com>
2025-03-07 07:17:06 +0530

    chage-logo

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+import box from "../Navbar/box.png"
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">MyLogo</div>
+      <div className="Name"> 
+      <img src={box} className="name-box"/>
+        <h4 className="name-h4">  ANIKET BHARDWAJ </h4>
+      </div>
       <div className="heading-1">
-        <h3> Home </h3>
-        <h3> Service </h3>
-        <h3> About </h3>
+        <h3> About Me </h3>
+        <h3> Resume </h3>
+        <h3> Projects </h3>
         <h3> Contact </h3>
       </div>
     </nav>
