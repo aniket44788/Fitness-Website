d26d02db77fab598f167e11cbab38ceea8f0f110
aniket44788 <darkpanda44788@gmail.com>
2025-03-15 21:26:50 +0530

    change-css

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-
+import profile from "../Images/profle.jpg"
 function Content() {
   return (
     <>
       <div className="content">
-        <div className="content-1">
           <div className="content-in">
+            <div className="img">
+              <img src={profile} alt=""className="img-img"/>
+            </div>
             <div className="content-in1"></div>
-            <div className="content-in2"></div>
           </div>
-        </div>
-        <div className="content-2"></div>
       </div>
     </>
   );
